9254bea21c3b43f7bd7d1783dd8fabd49b3d93b0
Paolo Scanferla <paolo.scanferla@gmail.com>
2014-03-17 21:35:50 +0100

    Removed placeholder in filter input field if FontAwesome is not used.

--- examples/leaderboard/leaderboard.js
+++ examples/leaderboard/leaderboard.js
@@ -15,7 +15,7 @@ if (Meteor.isClient) {
           { key: 'name', label: 'First Name', fn: function (name) { return name.split(' ')[0]; } },
           { key: 'score', label: 'Score'}
         ],
-		useFontAwesome: true
+        useFontAwesome: true
     };
   };
 
