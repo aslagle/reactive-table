96b5161e82cac4800caef5cf5e60cb49885d08c5
Amy Slagle <aslagle@ecohealth.io>
2014-10-02 17:09:44 -0400

    Don't set visible fields from collection if subscription isn't ready.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "A reactive table designed for Meteor",
-  version: "0.4.4",
+  version: "0.4.5",
   name: "aslagle:reactive-table",
   git: "https://github.com/ecohealthalliance/reactive-table.git"
 });
