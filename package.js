5dfaae7c8facceb980f25df2e01ae9c01134880e
Amy Slagle <aslagle@ecohealth.io>
2014-10-04 16:31:51 -0400

    Version 0.4.6.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "A reactive table designed for Meteor",
-  version: "0.4.5",
+  version: "0.4.6",
   name: "aslagle:reactive-table",
   git: "https://github.com/ecohealthalliance/reactive-table.git"
 });
