7f7ed7e066bd82da4f20925d06229aadd18044e3
physiocoder <ferro.roberto@email.it>
2014-10-06 12:34:05 +0200

    Added  argument to table, better styling of filter input

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  summary: "A reactive table designed for Meteor",
+  summary: "A reactive table designed for Meteor - Mathica fork",
   version: "0.4.7",
-  name: "aslagle:reactive-table",
-  git: "https://github.com/ecohealthalliance/reactive-table.git"
+  name: "mth:reactive-table",
+  git: "https://github.com/physiocoder/mth-reactive-table.git"
 });
 
 Package.on_use(function (api) {
