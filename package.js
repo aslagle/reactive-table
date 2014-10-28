f972a3cfd54115b8d6a222629aed1c83ccb05a9a
Amy Slagle <aslagle@ecohealth.io>
2014-10-27 21:01:14 -0400

    Ensure template data context is setup on re-renderings. (#116)

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "A reactive table designed for Meteor",
-  version: "0.5.0",
+  version: "0.5.1",
   name: "aslagle:reactive-table",
   git: "https://github.com/ecohealthalliance/reactive-table.git"
 });
