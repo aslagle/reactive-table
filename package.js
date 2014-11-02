953338aaa31dd25d88376a996d0012d3b7a8047d
Amy Slagle <aslagle@ecohealth.io>
2014-11-02 14:12:03 -0500

    Fix error when rendering in a context containing a 'fields' key.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "A reactive table designed for Meteor",
-  version: "0.5.4",
+  version: "0.5.5",
   name: "aslagle:reactive-table",
   git: "https://github.com/ecohealthalliance/reactive-table.git"
 });
--- test/test_template.js
+++ test/test_template.js
@@ -7,4 +7,13 @@ Tinytest.add('Template - nested', function (test) {
   test.length($('.reactive-table th'), 2, "two columns should be rendered");
   test.length($('.reactive-table tbody tr td:first-child'), 6, "six rows should be rendered and have cells");
   Blaze.remove(view);
+
+  var view2 = Blaze.renderWithData(
+    Template.testTmpl,
+    {collection: rows, array: [{fields: 'abc'}]},
+    document.body
+  );
+  test.length($('.reactive-table th'), 2, "two columns should be rendered");
+  test.length($('.reactive-table tbody tr td:first-child'), 6, "six rows should be rendered and have cells");
+  Blaze.remove(view2);
 });
