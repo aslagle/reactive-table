ecd9e5010929b31f30582c8356d7db9ce05120e5
Amy Slagle <aslagle@ecohealth.io>
2015-01-05 17:32:45 -0500

    Add test for user-editable settings not changing on reactive updates.

--- test/test_reactivity.js
+++ test/test_reactivity.js
@@ -79,3 +79,82 @@ testAsyncMulti('Reactivity - setting changed by helper', [function (test, expect
     Blaze.remove(view);
   }), 0);
 }]);
+
+testAsyncMulti('Reactivity - user-editable setting persists', [function (test, expect) {
+  var rowsPerPage = new ReactiveVar(2);
+  Template.testReactivity.helpers({
+    collection: function () {
+      return collection;
+    },
+    settings: function () {
+      return {rowsPerPage: rowsPerPage.get()};
+    }
+  });
+
+  var view = Blaze.renderWithData(
+    Template.testReactivity,
+    {},
+    document.body
+  );
+
+  test.length($('.reactive-table tbody tr'), 2, "two rows should be rendered");
+
+  var expectSameRowsPerPage = expect(function () {
+    test.length($('.reactive-table tbody tr'), 2, "two rows should be rendered");
+    Blaze.remove(view);
+  });
+
+  rowsPerPage.set(5);
+  Meteor.setTimeout(expectSameRowsPerPage, 0);
+}]);
+
+testAsyncMulti('Reactivity - user setting persists when other arguments change', [function (test, expect) {
+  var showCollection = new ReactiveVar(true);
+  Template.testReactivity.helpers({
+    collection: function () {
+      if (showCollection.get()) {
+        return collection;
+      } else {
+        return rows;
+      }
+    }, 
+    settings: function () {
+      return {rowsPerPage: 2};
+    }
+  });
+
+  var view = Blaze.renderWithData(
+    Template.testReactivity,
+    {},
+    document.body
+  );
+
+  test.equal($('.reactive-table tbody tr:first-child td:first-child').text(), "Ada Lovelace", "should be on first page");
+  test.length($('.reactive-table tbody tr'), 2, "first page should have two rows");
+  test.equal($('.reactive-table-navigation .page-number input').val(), "1", "displayed page number should be 1");
+
+  var testSecondPage = function () {
+    test.equal($('.reactive-table tbody tr:first-child td:first-child').text(), "Claude Shannon", "should be on the second page");
+    test.equal($('.reactive-table-navigation .page-number input').val(), "2", "displayed page number should be 2");
+  };
+
+  var expectStillSecondPage = expect(function () {
+      testSecondPage();
+      Blaze.remove(view);
+    });
+
+  var expectSecondPage = expect(function () {
+    testSecondPage();
+
+    showCollection.set(false);
+    Meteor.setTimeout(expectStillSecondPage, 0);
+  })
+
+  $('.reactive-table-navigation .page-number input').val('2');
+  $('.reactive-table-navigation .page-number input').trigger("change");
+
+  Meteor.setTimeout(expectSecondPage, 0);
+
+
+  showCollection.set(true);
+}]);
