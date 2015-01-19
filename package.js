7b5d873081ad3a6eaab3b39d068fafad272185a3
Amy Slagle <amslagle@gmail.com>
2015-01-19 11:48:31 -0500

    Fix row ids to handle consecutive subscriptions with overlapping rows, but different publication ids.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "A reactive table designed for Meteor",
-  version: "0.6.0-beta1",
+  version: "0.6.0-beta2",
   name: "aslagle:reactive-table",
   git: "https://github.com/aslagle/reactive-table.git"
 });
