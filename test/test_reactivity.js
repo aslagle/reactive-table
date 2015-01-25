ea99b11de4f62aab5e1baacab0080bd673392174
Amy Slagle <amslagle@gmail.com>
2015-01-25 17:11:23 -0500

    Fix reactivity with cursor argument.

--- test/test_reactivity.js
+++ test/test_reactivity.js
@@ -20,6 +20,28 @@ testAsyncMulti('Reactivity - collection', [function (test, expect) {
   }), 0);
 }]);
 
+testAsyncMulti('Reactivity - cursor', [function (test, expect) {
+  var collection = new Meteor.Collection();
+  collection.insert({name: 'item 1', value: 1});
+  var table = Blaze.renderWithData(
+    Template.reactiveTable,
+    {collection: collection.find()},
+    document.body
+  );
+  test.length($('.reactive-table tbody tr'), 1, "table should initially have one row");
+  collection.insert({name: 'item 2', value: 2});
+  test.length($('.reactive-table tbody tr'), 2, "table should reactively add second row");
+  collection.remove({name: 'item 2'});
+  test.length($('.reactive-table tbody tr'), 1, "table should reactively remove a row");
+
+  test.equal($('.reactive-table tbody tr:first-child td:nth-child(2)').text(), "1", "table row should have the initial value");
+  collection.update({name: 'item 1'}, {'$set': {value: 2}});
+  Meteor.setTimeout(expect(function () {
+    test.equal($('.reactive-table tbody tr:first-child td:nth-child(2)').text(), "2", "table should reactively update with new value");
+    Blaze.remove(table);
+  }), 0);
+}]);
+
 testAsyncMulti('Reactivity - collection changed by helper', [function (test, expect) {
   var collection = new Meteor.Collection();
   collection.insert({name: 'item 1', value: 1});
