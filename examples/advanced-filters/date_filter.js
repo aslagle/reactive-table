ed80f992fdfb60479de686ef2ededd0e80bebd38
Amy Slagle <amslagle@gmail.com>
2015-02-03 20:08:15 -0500

    Change date filter example to jquery ui's datepicker.

--- examples/advanced-filters/date_filter.js
+++ examples/advanced-filters/date_filter.js
@@ -2,10 +2,19 @@ if (Meteor.isClient) {
     Template.dateFilter.created = function () {
       this.filter = new ReactiveTable.Filter('date-filter', ['date']);  
     };
+  
+    Template.dateFilter.rendered = function () {
+      $('.date-selector').datepicker();
+    };
     
     Template.dateFilter.events({
        "change .date-selector": function (event, template) {
-           template.filter.set($(event.target).val());
+           if ($(event.target).val()) {
+             var date = moment($(event.target).val());
+             template.filter.set(date.format("YYYY-MM-DD"));
+           } else {
+             template.filter.set("");
+           }
        } 
     });
 }
