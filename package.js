e34e194cee98d9ccc05a97731d1b912aaebce319
Zibx <zibx.mail@gmail.com>
2015-03-04 04:40:37 +0400

    undo fix

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "A reactive table designed for Meteor",
-  version: "0.6.17",
+  version: "0.6.18",
   name: "zibx:reactive-table",
   git: "https://github.com/Zibx/reactive-table.git"
 });
