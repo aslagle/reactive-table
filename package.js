93cbcfbfff3b078786f48fc3a129a2a5268e9d88
Amy Slagle <amslagle@gmail.com>
2015-03-18 20:34:28 -0400

    Version 0.7.0: Custom filters.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "A reactive table designed for Meteor",
-  version: "0.7.0-beta2",
+  version: "0.7.0",
   name: "aslagle:reactive-table",
   git: "https://github.com/aslagle/reactive-table.git"
 });
