b1cff543948adddadb07a67e9c23be250fff38a0
Amy Slagle <amslagle@gmail.com>
2015-07-05 17:27:04 -0400

    Move column toggle checkboxes outside labels to prevent double click events. (#243)

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "A reactive table designed for Meteor",
-  version: "0.8.7",
+  version: "0.8.8",
   name: "aslagle:reactive-table",
   git: "https://github.com/aslagle/reactive-table.git"
 });
