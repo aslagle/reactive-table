1dfb3c2b77ffe7ef6fb35205d2892d85bd475e62
Vanson <vanson@vanportdev.com>
2016-01-17 16:21:17 +0800

    Fix crash

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "A reactive table designed for Meteor",
-  version: "0.8.231",
+  version: "0.8.232",
   name: "vansonhk:reactive-table",
   git: "https://github.com/VansonLeung/reactive-table.git"
 });
