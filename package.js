ca9e87055ab6384c1248b4ac2425f1ab0e0a683c
Amy Slagle <amslagle@gmail.com>
2016-02-13 17:58:17 -0500

    Version 0.8.25.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "A reactive table designed for Meteor",
-  version: "0.8.24",
+  version: "0.8.25",
   name: "aslagle:reactive-table",
   git: "https://github.com/aslagle/reactive-table.git"
 });
