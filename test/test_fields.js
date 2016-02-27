13e3d16922910428df692fbc896a360530265754
Amy Slagle <amslagle@gmail.com>
2016-02-27 15:57:37 -0500

    Fix handling of click events from label templates. (#361)

--- test/test_fields.js
+++ test/test_fields.js
@@ -167,6 +167,39 @@ Tinytest.add('Fields - label tmpl no data', function (test) {
   );
 });
 
+testAsyncMulti('Fields - label tmpl clickable to sort', [function (test, expect) {
+  var table = Blaze.renderWithData(
+    Template.reactiveTable,
+    {
+      collection: rows,
+      settings: {
+        fields: [
+          {key: "name", label: Template.testFieldsTmplNoData}
+        ]
+      }
+    },
+    document.body
+  );
+
+  var expectAscending = expect(function () {
+    test.equal($('.reactive-table tbody tr:first-child td:first-child').text(), "Ada Lovelace", "ascending first row");
+    test.equal($('.reactive-table tbody tr:nth-child(2) td:first-child').text(), "Carl Friedrich Gauss", "ascending second row");
+    test.equal($('.reactive-table tbody tr:nth-child(4) td:first-child').text(), "Grace Hopper", "ascending fourth row");
+    Blaze.remove(table);
+  });
+
+  var expectDescending = expect(function () {
+    test.equal($('.reactive-table tbody tr:first-child td:first-child').text(), "Nikola Tesla", "descending first row");
+    test.equal($('.reactive-table tbody tr:nth-child(2) td:first-child').text(), "Marie Curie", "descending second row");
+    test.equal($('.reactive-table tbody tr:nth-child(4) td:first-child').text(), "Claude Shannon", "descending fourth row");
+
+    $('span.test').click();
+    Meteor.setTimeout(expectAscending, 0);
+  });
+
+  $('span.test').click();
+  Meteor.setTimeout(expectDescending, 0);
+}]);
 
 Tinytest.add('Fields - header class string', function (test) {
   testTable(
