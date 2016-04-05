fb3f6176ba3efec67a709361c0fb31586494d37c
Clarence Liu <clarence.hl.liu@gmail.com>
2016-04-04 21:25:03 -0700

    delete examples

--- examples/advanced-filters/clear_filters_button.js
+++
@@ -1,9 +0,0 @@
-if (Meteor.isClient) {
-
-    Template.clearFiltersButton.events({
-       "click #clear-filters-button" : function () {
-           ReactiveTable.clearFilters(['filter1', 'filter2', 'filter3', 'date-filter', 'checkbox-filter']);
-       }
-    });
-    
-}
